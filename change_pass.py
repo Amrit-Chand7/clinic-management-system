503ff5d785f5c0cec70a4258aeadffac83608a49
Amrit-Chand7 <chandamrit62@gmail.com>
2025-07-20 12:57:28 +0545

    change_pass.py connect with admin_interface.py and add database

--- change_pass.py
+++ change_pass.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+import sqlite3
 
 # Function to handle password update
 def change_password():
@@ -11,6 +12,12 @@ def change_password():
     password_window.resizable(False, False)
 
     # Labels and Entry fields
+    num1_label = Label(password_window, text="Number:", bg="light blue", font=("Arial", 15),fg="black")
+    num1_label.place(x=65, y=20)
+    num1_entry = Entry(password_window, font=("Arial", 12),bg="white", fg="black", width=25)
+    num1_entry.place(x=225, y=20)
+
+
     old_label = Label(password_window, text="Old Password:", bg="light blue", font=("Arial", 15),fg="black")
     old_label.place(x=50, y=60)
     old_pass_entry = Entry(password_window, font=("Arial", 12),bg="white", fg="black", width=25, show="*")
@@ -25,28 +32,48 @@ def change_password():
     confirm_label.place(x=50, y=140)
     confirm_pass_entry = Entry(password_window, font=("Arial", 12), bg="white", fg="black", width=25, show="*")
     confirm_pass_entry.place(x=225, y=140)
-
-
-    old_password = old_pass_entry.get()
-    new_password = new_pass_entry.get()
-    confirm_password = confirm_pass_entry.get()
-
-    # Validate if all fields are filled
-    if old_password and new_password and confirm_password:
-        if new_password == confirm_password:
-            messagebox.showinfo("Password Change Successful", "Your password has been changed!")
-        else:
-            messagebox.showwarning("Error", "New passwords do not match!")
     
     # Function to handle cancellation
     def save():
-        result = messagebox.askyesno("Save", "Are you sure you want to change your password?")
-        if result==True:
-            messagebox.showinfo("Success", "Password changed successfully!")
-            password_window.destroy()
+        phone_n = num1_entry.get()
+        old_password = old_pass_entry.get()
+        new_password = new_pass_entry.get()
+        confirm_password = confirm_pass_entry.get()
 
+        if not phone_n or not old_password or not new_password or not confirm_password:
+            messagebox.showwarning("Warning", "All fields are required")
+            return
+        
+        if new_password != confirm_password:
+                messagebox.showerror("Error", "New passwords do not match")
+                return
+        
+        conn = sqlite3.connect("clinic_management_system.db")
+        c = conn.cursor()
+        c.execute("SELECT password FROM admin WHERE phone = ?", (phone_n,))
+        result = c.fetchone()
+        conn.close()
+        if result == None:
+            messagebox.showerror("Error", "Phone number not found")
+            return
+        
+        if result[0] != old_password:
+            messagebox.showerror("Error", "Old password is incorrect")
+            return
+        
         else:
-            messagebox.showinfo("Cancelled", "Password change cancelled.")
+            result = messagebox.askyesno("Save", "Are you sure you want to change your password?")
+            
+            if result==True:
+                conn = sqlite3.connect("clinic_management_system.db")
+                c = conn.cursor()
+                c.execute("UPDATE admin SET password = ? WHERE phone = ?", (new_password, phone_n))
+                conn.commit()
+                conn.close()
+                messagebox.showinfo("Success", "Password changed successfully!")
+                password_window.destroy()
+            else:
+                messagebox.showinfo("Cancelled", "Password change cancelled.")
 
     save_btn = Button(password_window, text="Save", font=("Arial", 12), width=12, command=save)
     save_btn.place(x=130, y=210)
